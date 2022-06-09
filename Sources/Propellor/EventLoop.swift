1ec186c76954730729059b82b28b03e80339ea3a
Ryan Walklin <ryan@testtoast.com>
2022-06-09 22:02:47 +1200

    Start implementing FD-based event loop handling

--- Sources/Propellor/EventLoop.swift
+++ Sources/Propellor/EventLoop.swift
@@ -6,30 +6,48 @@ class EventLoop {
 
     var handleEvent: ((Event) -> Void)? = nil
 
-    private var _running = false
-
-    private let _eventQueue: DispatchQueue
-
-    init () {
-        _eventQueue = DispatchQueue(label: "com.testtoast.spitfire.terminalevents")
-    }
+    private var _ttyDispatchSource: DispatchSourceRead? = nil
+    private var _resizeDispatchSource: DispatchSourceRead? = nil
 
     func start () {
-        _running = true
-        _eventQueue.async {
-            while self._running {
-                let event = Termbox.peekEvent(timeout: 100)
-                if event == .timeout {
-                    continue
-                }
-                DispatchQueue.main.async {
+        if _ttyDispatchSource != nil || _resizeDispatchSource != nil {
+            stop()
+        }
+        let fds = Termbox.getFDs()
+        _ttyDispatchSource = DispatchSource.makeReadSource(fileDescriptor: fds.tty, queue: DispatchQueue.main)
+        _resizeDispatchSource = DispatchSource.makeReadSource(fileDescriptor: fds.resize, queue: DispatchQueue.main)
+
+        let eventHandler = DispatchWorkItem() {
+            do {
+                repeat {
+                    let event = try Termbox.peekEvent(timeout: 0)
                     self.handleEvent?(event)
+                } while true
+            } catch let error as TermboxError {
+                switch error {
+                case .noEvent:
+                    return
+                default:
+                    Log.error(error.description)
                 }
+            } catch let error {
+                Log.error(error.localizedDescription)
             }
         }
+
+        _ttyDispatchSource?.setEventHandler(handler: eventHandler)
+        _resizeDispatchSource?.setEventHandler(handler: eventHandler)
+
+        _ttyDispatchSource?.activate()
+        _resizeDispatchSource?.activate()
+
+        Log.debug("termbox: event handler activated")
     }
 
     func stop () {
-        _running = false
+        _ttyDispatchSource?.cancel()
+        _resizeDispatchSource?.cancel()
+        _ttyDispatchSource = nil
+        _resizeDispatchSource = nil
     }
 }
